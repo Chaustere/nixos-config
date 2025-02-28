bbd85328b303f9437b4353fb21b03d394ee8b8f4
Chaustere <besnard.owen@gmail.com>
2025-03-01 00:07:29 +0100

    fix: linked dotfiles individually

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -11,7 +11,18 @@
 		# ./fish.nix
 		# ./yazi.nix
 	];
-
-	home.file.".config".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles";
-
+	
+	### vvv  bad idea  vvv
+	# home.file.".config".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles";
+	
+	home.file.".config/sway".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles/sway";
+	home.file.".config/swaync".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles/swaync";
+	home.file.".config/swaylock".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles/swaylock";
+	home.file.".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles/waybar";
+	home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles/nvim";
+	home.file.".config/rofi".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles/rofi";
+	home.file.".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles/kitty";
+	home.file.".config/fish".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles/fish";
+	home.file.".config/yazi".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/sway-dotfiles/yazi";
+	
 }
