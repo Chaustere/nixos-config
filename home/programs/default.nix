2bc2cac67a0b9dcf1ffd09621875a7fffccb1396
Chaustere <besnard.owen@gmail.com>
2025-03-01 01:07:18 +0100

    fix: tried fixing .config symlinks again

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -2,7 +2,9 @@
 	config,
 	dotfiles,
 	...
-}: {
+}: let
+	dotfilesPath = "${config.home.homeDirectory}/NixOS-Config/nixos-config/home/dotfiles";
+in {
 	imports = [
 		./common.nix
 		./media.nix
@@ -13,16 +15,16 @@
 	];
 	
 	### vvv  bad idea  vvv
-	# home.file.".config".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles;
+	# home.file.".config".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/dotfiles";
 	
-	home.file.".config/sway".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles/sway;
-	home.file.".config/swaync".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles/swaync;
-	home.file.".config/swaylock".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles/swaylock;
-	home.file.".config/waybar".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles/waybar;
-	home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles/nvim;
-	home.file.".config/rofi".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles/rofi;
-	home.file.".config/kitty".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles/kitty;
-	home.file.".config/fish".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles/fish;
-	home.file.".config/yazi".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles/yazi;
+	home.file.".config/sway".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/sway";
+	home.file.".config/swaync".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/swaync";
+	home.file.".config/swaylock".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/swaylock";
+	home.file.".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/waybar";
+	home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/nvim";
+	home.file.".config/rofi".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/rofi";
+	home.file.".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/kitty";
+	home.file.".config/fish".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/fish";
+	home.file.".config/yazi".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/yazi";
 	
 }
