59e82b244031bbd1c9e3890cf14dd491712fc7aa
Chaustere <besnard.owen@gmail.com>
2025-03-03 00:14:38 +0100

    feat: set up gammastep

--- home/display/default.nix
+++ home/display/default.nix
@@ -7,6 +7,7 @@
 		./swaync.nix
 		./waybar.nix
 		./rofi.nix
+		./gammastep.nix
 	];
 	### vvv  bad idea  vvv
 	# home.file.".config".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/dotfiles";
