c9533db892d17331bcef2424c3f14480f8287d01
Chaustere <besnard.owen@gmail.com>
2025-03-02 14:52:21 +0100

    feat: added starship config file

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -1,16 +1,22 @@
 {
+	config,
+	dotfilesPath,
+	...
+}: {
   programs.starship = {
     enable = true;
     # Configuration written to ~/.config/starship.toml
-    settings = {
-      # add_newline = false;
-
-      # character = {
-      #   success_symbol = "[➜](bold green)";
-      #   error_symbol = "[➜](bold red)";
-      # };
-
-      # package.disabled = true;
-    };
+    # settings = {
+    #   # add_newline = false;
+    #
+    #   # character = {
+    #   #   success_symbol = "[➜](bold green)";
+    #   #   error_symbol = "[➜](bold red)";
+    #   # };
+    #
+    #   # package.disabled = true;
+    # };
   };
+
+  home.file.".config/starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/starship/starship.toml";
 }
