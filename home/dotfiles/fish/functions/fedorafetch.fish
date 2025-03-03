84ce06b35175c69ce69e40859dbb9c7b5849b49d
Chaustere <besnard.owen@gmail.com>
2025-03-03 03:03:47 +0100

    feat: nixosfetch

--- home/dotfiles/fish/functions/fedorafetch.fish
+++
@@ -1,4 +0,0 @@
-function fedorafetch
-    fastfetch --kitty-direct ~/Images/nixos --logo-width 35 --logo-height 19
-	echo (ll ~/Images/nixos.png)
-end
---
+++ home/dotfiles/fish/functions/nixosfetch.fish
@@ -0,0 +1,3 @@
+function nixosfetch
+    fastfetch --kitty-direct $HOME/Images/nixos.png --logo-width 45 --logo-height 19
+end
